db4fbebff94249fe0091458d4506b6385440bca0
Ekiru Timothy <ekiru@outlook.com>
2017-03-13 22:49:54 +0300

    Init basic placeholder page

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div  className="jumbo">
+          <h1>AdayJobs</h1>
+          <p>info@adayjobs.com | (857) 250-0995</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;

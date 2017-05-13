af2acb9000d1f12b22fc0dded34f676e43bb671f
Ekiru Timothy <ekiru@outlook.com>
2017-05-13 11:29:08 +0300

    changed scheduler's event colors to specified

--- src/components/scheduling/index.js
+++ src/components/scheduling/index.js
@@ -44,11 +44,11 @@ export default class Schedule extends Component {
 		var TODAY = todayDate.format('YYYY-MM-DD');
 		var TOMORROW = todayDate.clone().add(1, 'day').format('YYYY-MM-DD');
 		let events = [
-			{ id: '1', resourceId: 'b', start: TODAY + 'T02:00:00', end: TODAY + 'T07:00:00', title: 'event 1' },
-			{ id: '2', resourceId: 'c', start: TODAY + 'T05:00:00', end: TODAY + 'T22:00:00', title: 'event 2' },
-			{ id: '3', resourceId: 'd', start: YESTERDAY, end: TOMORROW, title: 'event 3' },
-			{ id: '4', resourceId: 'e', start: TODAY + 'T03:00:00', end: TODAY + 'T08:00:00', title: 'event 4' },
-			{ id: '5', resourceId: 'f', start: TODAY + 'T00:30:00', end: TODAY + 'T02:30:00', title: 'event 5' }
+			{ id: '1', resourceId: 'b', start: TODAY + 'T02:00:00', end: TODAY + 'T07:00:00', title: 'event 1', color: '#E03A2C' },
+			{ id: '2', resourceId: 'c', start: TODAY + 'T05:00:00', end: TODAY + 'T22:00:00', title: 'event 2', color: '#0F7F12' },
+			{ id: '3', resourceId: 'd', start: YESTERDAY, end: TOMORROW, title: 'event 3', color: '#0F7F12' },
+			{ id: '4', resourceId: 'e', start: TODAY + 'T03:00:00', end: TODAY + 'T08:00:00', title: 'event 4', color: '#E03A2C' },
+			{ id: '5', resourceId: 'f', start: TODAY + 'T00:30:00', end: TODAY + 'T02:30:00', title: 'event 5', color: '#FDAC43' }
 		]
 		let header = {
 			left:   'prev,next',

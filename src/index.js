c65cb284de3634a4be50b21da54c1cb634c13ecf
coglite <info.mrjsingh@gmail.com>
2017-07-21 04:37:06 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
+import ApolloClient, { createNetworkInterface } from 'apollo-client'
+import { ApolloProvider } from 'react-apollo'
 import { renderRoutes } from 'react-router-config';
 
 //import App from './App';
@@ -16,11 +17,17 @@ import './index.css';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
+const client = new ApolloClient({
+   networkInterface: createNetworkInterface({ uri: 'https://forward-chess-157313.appspot.com/graphql'}),
+ })
+
 ReactDOM.render(
   <MuiThemeProvider>
+	<ApolloProvider client={client}>
 	  <Router>
 	    {renderRoutes(routes)}
 	  </Router>
+        </ApolloProvider>
   </MuiThemeProvider>,
   document.getElementById('root')
 );

322b0fc5e959af14b647ca1d4582a762b3e7f5c7
Rahkeem Morris <rahkeem.morris+github@gmail.com>
2017-08-31 22:23:31 -0400

    Updating map to match workplace prototype design

--- src/components/maps/Workplace.js
+++ src/components/maps/Workplace.js
@@ -3,14 +3,13 @@ import { Button } from 'semantic-ui-react';
 import '../Scheduling/style.css';
 import { withGoogleMap, GoogleMap, Marker } from "react-google-maps";
 import _ from 'lodash';
-
 import './maps.css'
 
 const WorkplaceGoogleMap = withGoogleMap(props => (
   <GoogleMap
     ref={props.onMapLoad}
-    defaultZoom={5}
-    defaultCenter={{ lat: -25.363882, lng: 131.044922 }}
+    defaultZoom={15}
+    defaultCenter={{ lat: 42.364109, lng: -71.124184 }}
     onClick={props.onMapClick}
   >
     {props.markers.map((marker, index) => (
@@ -29,20 +28,29 @@ export default class WorkplaceMap extends Component {
 			<div className="workplace-map">
 				<WorkplaceGoogleMap
 			    containerElement={
-			      <div style={{ height: '80%', padding:7}} />
+			      <div style={{ height: '72%', padding:5}} />
 			    }
 			    mapElement={
-			      <div style={{ height: '100%', borderWidth: 2, borderColor:'#4A4A4A'}} />
+			      <div className="map-element" />
 			    }
+
 			    onMapLoad={_.noop}
 			    onMapClick={_.noop}
 			    markers={[]}
 			    onMarkerRightClick={_.noop}
 			  />
+
 				{/*<div> {this.props.address} </div>*/}
-				<Button className="btn-image" style={{marginLeft:5}}>
-					Get Directions
-				</Button>
+                {/*hard coded, to be updated later ... */}
+                <div style={{marginLeft:10, marginTop: 5}}>
+                    <text className="address">25 Harvard Way</text>
+                    <br></br>
+                    <text className="address">Boston, MA 02163</text>
+                </div>
+
+				<a className="button"  href="https://goo.gl/maps/iX5b61ZiSr72" target="_newtab">
+					<text className="btn-text">GET DIRECTIONS</text>
+				</a>
 			</div>
 		);
 	}

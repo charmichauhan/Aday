c42c8c3bc1d080ea73fe0d5940091d55133ead31
kendallzhu <kzhu@college.harvard.edu>
2017-10-29 16:11:14 -0400

    reconfigured resume page to recieve data in database format

--- src/components/Team/ProfileDrawer/ResumeDrawer.js
+++ src/components/Team/ProfileDrawer/ResumeDrawer.js
@@ -33,112 +33,152 @@ class ResumeDrawerComponent extends Component {
     } = this.props;
 
     let userDetails = this.props.userQuery && this.props.userQuery.userById;
+    // version to load data from query
 
+    const person = {
+      experience: userDetails.userEmployersByUserId.nodes,
+      education: userDetails.userEducationsByUserId.nodes,
+      reference: userDetails.userReferencesByUserId.nodes,
+      availability: userDetails.userAvailabilitiesByUserId.nodes,
+      language: userDetails.userLanguagesByUserId.nodes,
+      address: userDetails.homeAddress ? JSON.parse(userDetails.homeAddress).home_address[0] : ""
+    };
+
+    // hard-coded version
+    /*
     const person = {
       experience: [
         {
-          companyLogo: 'https://seeklogo.com/images/D/dunkin-donuts-logo-1E269BA8F1-seeklogo.com.png',
+          employerName: 'Dunkin Donuts',
+          city: 'Cambridge',
+          state: 'MA',
           jobTitle: 'Crew Member',
-          company: 'Dunkin Donuts',
-          startDate: 'Jul 2012',
-          endDate: 'Jul 2015',
-          jobDuration:'3 yrs 1 mo',
-          jobLocation:'Cambridge, MA',
-          jobDescription: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.'
+          jobDescription: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.',
+          startDate: moment('Jul 2012', 'MMM-YYYY').toISOString(),
+          endDate: moment('Jul 2015', 'MMM-YYYY').toISOString(),
+          // not in table
+          companyLogo: 'https://seeklogo.com/images/D/dunkin-donuts-logo-1E269BA8F1-seeklogo.com.png'
         },
         {
-          companyLogo: 'https://lh3.googleusercontent.com/SXFAotyIHfIJwL05WxIe_yjhWDTkBUK5n2uAB4eNsW2sCDqyhdoytJU2xeyM8NIksMkt63SoE2Pq2g=w2560-h1440-rw-no',
+          employerName: 'Wal-mart',
+          city: 'Malden',
+          state: 'MA',
           jobTitle: 'Crew Member',
-          company: 'Wal-mart',
-          startDate: 'Jan 2010',
-          endDate: 'Aug 2012',
-          jobDuration:'2 yrs 6 mo',
-          jobLocation:'Malden, MA',
-          jobDescription: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.'
+          jobDescription: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.',
+          // reformat
+          startDate: moment('Jan 2010', 'MMM-YYYY').toISOString(),
+          endDate: moment('Aug 2012', 'MMM-YYYY').toISOString(),
+          // not in table
+          companyLogo: 'https://lh3.googleusercontent.com/SXFAotyIHfIJwL05WxIe_yjhWDTkBUK5n2uAB4eNsW2sCDqyhdoytJU2xeyM8NIksMkt63SoE2Pq2g=w2560-h1440-rw-no'
         },
         {
-          companyLogo: '/assets/Icons/default-workplace.png',
+          employerName: 'Freelance',
+          city: 'Malden',
+          state: 'MA',
           jobTitle: 'Bartender',
-          company: 'Freelance',
-          startDate: 'Jan 2010',
-          endDate: 'Aug 2012',
-          jobDuration:'2 yrs 6 mo',
-          jobLocation:'Boston, MA',
-          jobDescription: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.'
+          jobDescription: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.',
+          // reformat
+          startDate: moment('Jan 2010', 'MMM-YYYY').toISOString(),
+          endDate: moment('Aug 2012', 'MMM-YYYY').toISOString(),
+          // not in table
+          companyLogo: '/assets/Icons/default-workplace.png'
         },
       ],
       education: [
         {
+          educationalInstitutionName: 'City of Boston High School',
+          city: 'Boston',
+          state: 'MA',
+          awardType: 'High School Diploma',
+          fieldOfStudy: null,
+          // reformat
+          startDate: moment('Sep 2006', 'MMM-YYYY').toISOString(),
+          endDate: moment('May 2010', 'MMM-YYYY').toISOString(),
+          // not in table
           institutionLogo: '/assets/Icons/default-education.png',
-          institutionName: 'City of Boston High School',
-          awardName: 'High School Diploma',
-          startDate: 'Sep 2006',
-          endDate: 'May 2010',
           awardDescription: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.'
         },
         {
+          educationalInstitutionName: 'Opportunity Advancement Innovation',
+          city: 'Boston',
+          state: 'MA',
+          awardType: 'Construction Industry Safety Course',
+          fieldOfStudy: null,
+          // reformat
+          startDate: moment('Jan 2012', 'MMM-YYYY').toISOString(),
+          endDate: moment('Jul 2012', 'MMM-YYYY').toISOString(),
+          // not in table
           institutionLogo: 'http://www.marinelog.com/media/k2/items/cache/ad0ec80b2a19407fd396c1c0067174a0_L.jpg?t=943938000',
-          institutionName: 'Opportunity Advancement Innovation',
-          awardName: 'Construction Industry Safety Course',
-          startDate: 'Jan 2012',
-          endDate: 'Jul 2012',
           awardDescription: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.'
         },
       ],
       reference: [
         {
-          avatar: 'https://s3.us-east-2.amazonaws.com/harvard-operations-pics/Operations/RA/albeiro-restrepo.jpg',
           firstName: 'Albeiro',
           lastName: 'Restrepo',
-          phoneNumber: '1-617-839-9830',
-          referenceDate: 'Jul 15, 2017',
+          referencePhoneNumber: '1-617-839-9830',
+          // reformat
+          referenceEmailAddress: '',
           relationship: 'Worked together at Dunkin Donuts',
+
+          // not in table
+          referenceDate: moment('Jul 2017', 'MMM-YYYY').toISOString(),
+          avatar: 'https://s3.us-east-2.amazonaws.com/harvard-operations-pics/Operations/RA/albeiro-restrepo.jpg',
           referenceText: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.'
         },
         {
-          avatar: '/assets/Icons/default-reference.png',
           firstName: 'Anna',
           lastName: 'Reyes',
-          phoneNumber: '1-617-948-1049',
-          referenceDate: 'Jul 23, 2017',
+          referencePhoneNumber: '1-617-948-1049',
+          // reformat
+          referenceEmailAddress: '',
           relationship: 'Worked together at Wal-mart',
+
+          // not in table
+          referenceDate: moment('Jul 2017', 'MMM-YYYY').toISOString(),
+          avatar: '/assets/Icons/default-reference.png',
           referenceText: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.'
         },
       ],
-      availability: 'Part-Time',
-      address: {
-        line1: '249 Main Street',
-        line2: 'Apt B',
-        line3: 'Malden, MA 03813'
-      },
+      availability: [
+        {
+          hourRange: "part-time"
+        }
+      ],
+      address: {address_line1:"249 Main Street",
+                address_line2:"Apt B",
+                city:"Malden",
+                state:"MA"},
       language: [
         {
-          language: 'English',
+          languageName: 'English',
+          // not in table
           proficiency: 'Native',
         },
         {
-          language: 'Spanish',
+          languageName: 'Spanish',
+          // not in table
           proficiency: 'Native',
         },
       ],
+      // not in table (?)
       certificate: [
         {
           name: 'FrontEnd Developer',
           institution: 'Platzi',
-          date: 'Jan 2015',
+          date: moment('Jan 2015', 'MMM-YYYY').toISOString(),
           description: 'Aenean commodo ligula eget dolor. Aenean massa.'
         },
         {
           name: 'Backend Developer',
           institution: 'Platzi',
-          date: 'Jan 2016',
+          date: moment('Jan 2016', 'MMM-YYYY').toISOString(),
           description: 'Aenean commodo ligula eget dolor. Aenean massa.'
         }
       ]
     };
-
+    */
     return (
-
       <Drawer docked={docked} width={width}
               openSecondary={openSecondary}
               onRequestChange={this.props.backProfileDrawer}
@@ -148,23 +188,23 @@ class ResumeDrawerComponent extends Component {
             <div className="drawer-heading col-md-12" >
               <FlatButton label="Back" onClick={this.props.backProfileDrawer}
                           icon={<Icon name="chevron left" className="floatLeft" style={{fontSize: 26}} /> }
-                          style={{position: 'absolute', left: '15px', color: '#E33821', fontSize: 18, paddingLeft: 0}}
+                          style={{position: 'absolute', left: '15px', color: 'red', fontSize: 18, paddingLeft: 0}}
               />
 
-              <span className="profile-title first-name-title" style={{color:"#0021A1"}}>{userDetails.firstName}</span>
+              <span className="profile-title first-name-title" style={{color:"blue"}}>{userDetails.firstName}</span>
               &nbsp;&nbsp;
-              <span className="profile-title last-name-title" style={{color:"#0021A1"}}>{userDetails.lastName} </span>
+              <span className="profile-title last-name-title" style={{color:"blue"}}>{userDetails.lastName} </span>
 
               {userDetails.avatarUrl
-                     ? <Image centered='true' size='small' shape='circular' className="profile-img" src={ userDetails.avatarUrl}/>
-                     :  <Image centered='true' size='small' shape='circular' className="profile-img"
+                     ? <Image size='small' shape='circular' className="profile-img" src={ userDetails.avatarUrl}/>
+                     :  <Image size='small' shape='circular' className="profile-img"
                           src="https://s3.us-east-2.amazonaws.com/aday-website/anonymous-profile.png"/>
               }
             </div>
 
             <div className="resume-subject text-center">
               <img src="/assets/Icons/down-quotes.png" className="quotation-mark down" style={{width:30}} />
-              <span className="tagline">I'm looking for opportunities in the construction industry and to develop my career</span>
+              <span className="tagline">I{`'`}m looking for opportunities in the construction industry and to develop my career</span>
               <img src="/assets/Icons/up-quotes.png" className="quotation-mark up" style={{width:30}} />
             </div>
 
@@ -199,7 +239,7 @@ class ResumeDrawerComponent extends Component {
                       <div className="section-content" style={{width:640}}>
                         <div className="resume-image-lane" />
                         <div style={{flex:11}}>
-                          <p className="resume-h2">{person.availability}</p>
+                          <p className="resume-h2">{person.availability[0] ? person.availability[0].hourRange : ''}</p>
                         </div>
                       </div>
                     </div>
@@ -210,18 +250,20 @@ class ResumeDrawerComponent extends Component {
                     <Image src="/assets/Icons/house.png" className="resume-icon"/>
                     <div>
                       <span className="resume-title">Home Address</span>
-                      <p className="resume-h4" style={{fontStyle:'italic'}}>As an employer, you have access to the team member's home address</p>
+                      <p className="resume-h4" style={{fontStyle:'italic'}}>As an employer, you have access to the team member{`'`}s home address</p>
                     </div>
                   </div>
                   <div className="section-item">
                     <div className="section-subitem">
                       <div className="section-content" style={{width:640}}>
                         <div className="resume-image-lane" />
-                        <div style={{flex:11}}>
-                          <p className="resume-h2">{person.address.line1}</p>
-                          <p className="resume-h2">{person.address.line2}</p>
-                          <p className="resume-h2">{person.address.line3}</p>
-                        </div>
+                        {person.address &&
+                          <div style={{flex:11}}>
+                            <p className="resume-h2">{person.address.address_line1}</p>
+                            <p className="resume-h2">{person.address.address_line2}</p>
+                            <p className="resume-h2">{person.address.city + ", " + person.address.state}</p>
+                          </div>
+                        }
                       </div>
                     </div>
                   </div>
